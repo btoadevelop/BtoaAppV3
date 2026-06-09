f2b289111c7ad36ef0fe0cb00eb0f26955a91fec
btoadevelop <btoadevelop@gmail.com>
2026-06-10 06:12:39 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,3 +1,10 @@
+// Import modul Firebase
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Konfigurasi Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyA8Pqxr7-S2cUBTBMAxUGknsV3UmLxTH0o",
   authDomain: "btoaappv3.firebaseapp.com",
@@ -7,5 +14,11 @@ const firebaseConfig = {
   appId: "1:617767588735:web:0bb8de934b9a3f7d53004a",
   measurementId: "G-LSWRFR0MLQ"
 };
+
+// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
